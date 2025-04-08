1d817fedb8b12090a2ec062508b7a59dbb5b012a
Oskar Hansen <oskarmelorm@gmail.com>
2025-04-08 14:51:17 +0200

    haha

--- Empire.Shared/Models/DTOs/RawDeckEntry.cs
+++ Empire.Shared/Models/DTOs/RawDeckEntry.cs
@@ -1,10 +1,14 @@
 ﻿namespace Empire.Shared.Models.DTOs
 {
+    using MongoDB.Bson.Serialization.Attributes;
+
+    [BsonIgnoreExtraElements]
     public class RawDeckEntry
     {
         public int CardId { get; set; }
         public int Count { get; set; }
+        public string DeckType { get; set; } = string.Empty;
         public string Player { get; set; } = string.Empty;
-        public string DeckType { get; set; } // "Civic" or "Military"
     }
+
 }
